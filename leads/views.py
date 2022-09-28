df5c4bf5c64558d8aa35ec81d0a5dd8abcd20cf8
imadbnc <imadbrnchenouf@gmail.com>
2022-09-27 23:40:01 -0400

    add a new model

---
+++ leads/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Lead
+
+
+def home_page(request):
+    #return HttpResponse("hello world")
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+
+    }
+    return render(request, "second_page.html",context) 
