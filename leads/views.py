4f761175b6f8cc509f9954b0abc8c651e9edf981
imadbnc <imadbrnchenouf@gmail.com>
2022-10-09 15:20:40 -0400

    update the code

--- leads/views.py
+++ leads/views.py
@@ -86,6 +86,9 @@ class LeadCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
         return reverse("leads:lead-list")
     
     def form_valid(self, form):
+        lead = form.save(commit=False)
+        lead.organisation = self.request.user.userprofile
+        lead.save()
         # TO send email
         send_mail(
             subject =" A lead has been created",
