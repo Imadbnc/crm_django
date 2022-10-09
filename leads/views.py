ce5f5af81dfad9e7df0cdfff3bd232396b4cab5c
imadbnc <imadbrnchenouf@gmail.com>
2022-10-09 12:05:47 -0400

    Add a third party package Django-crispy-forms

--- leads/views.py
+++ leads/views.py
@@ -241,7 +241,7 @@ class LeadCategoryUpdateView(LoginRequiredMixin, generic.UpdateView):
         return queryset
 
     def get_success_url(self):
-        return reverse("leads:lead-list")
+        return reverse("leads:lead-detail", kwargs={"pk" : self.get_object().id })
 
 
 
