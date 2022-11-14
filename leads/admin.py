1ad024d567416eca7b9e464e880fb9fe33a44a53
imadbnc <imadbrnchenouf@gmail.com>
2022-11-13 23:18:36 -0500

    upload images

--- leads/admin.py
+++ leads/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import User, Lead, Agent , UserProfile, Category  
 
+# class LeadAdmin(admin.ModelAdmin):
+#     fields=(
+#         'first_name',
+#         'last_name',
+#     )
+
 admin.site.register(Category)
 admin.site.register(User)
 admin.site.register(UserProfile)
